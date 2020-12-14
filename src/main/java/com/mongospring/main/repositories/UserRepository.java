937871f81fbcd5294230e8b8b36bb38f4cb6512f
Higor Pereira <madhigor@hotmail.com>
2020-12-14 05:04:47 -0300

    Fixing type mistake

--- src/main/java/com/mongospring/main/repositories/UserRepository.java
+++ src/main/java/com/mongospring/main/repositories/UserRepository.java
@@ -4,8 +4,9 @@ import com.mongospring.main.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.io.Serializable;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Serializable> {
+public interface UserRepository extends MongoRepository<User, String> {
+
+
 }
