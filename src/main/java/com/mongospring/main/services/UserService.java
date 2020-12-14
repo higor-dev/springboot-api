3fd4bd5e22b1e47c0a354cbd0da8b1e54983563a
Higor Pereira <madhigor@hotmail.com>
2020-12-14 05:43:30 -0300

    Creating User findById endpoint.

--- src/main/java/com/mongospring/main/services/UserService.java
+++ src/main/java/com/mongospring/main/services/UserService.java
@@ -2,10 +2,12 @@ package com.mongospring.main.services;
 
 import com.mongospring.main.domain.User;
 import com.mongospring.main.repositories.UserRepository;
+import com.mongospring.main.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("User not found."));
     }
 
 }
